e75631c0bb12212a400e98b2a395f3d3022af68e
dbursztynowski <dariusz.bursztynowski@orange.com>
2021-04-16 14:50:09 +0000

    adding topic monitoring to policy-enf.py

--- mme/VESprepareSendEvent.py
+++ mme/VESprepareSendEvent.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys
+import sys, time
 #import requests
 
 #test input params
@@ -18,6 +18,12 @@ print("preparing VES telemetry event")
 fil = open("VES_send_template.json", "r")
 f=fil.read()
 
+#set lastEpochMicrosec
+i = f.find('lastEpochMicrosec')
+cl0= f.find(':',i)
+cm0=f.find(',',cl0)
+lastEpoch = int(time.time()*1000000)
+
 #set reporting
 i = f.find('reportingEntityName')
 cl1= f.find(':',i)
@@ -28,12 +34,26 @@ i = f.find('sourceName')
 cl2= f.find(':',i)
 cm2=f.find(',',cl2)
 
-#set value
+#set value receivedPackets
 i = f.find('receivedTotalPacketsDelta')
+cl4= f.find(':',i)
+cm4=f.find(',',cl4)
+
+#set startEpochMicrosec
+i = f.find('startEpochMicrosec')
 cl3= f.find(':',i)
 cm3=f.find(',',cl3)
+startEpoch = int(time.time()*1000000)
 
-evnt=f[:cl1] + ':\"' + sys.argv[1] + '\"' + f[cm1:cl2] + ':\"' + sys.argv[1] + '\"' + f[cm2:cl3] + ':' + str(sys.argv[2]) + f[cm3:]
+#set value receivedTotalPackets
+i = f.find('receivedTotalPacketsDelta')
+cl4= f.find(':',i)
+cm4=f.find(',',cl4)
+
+evnt = f[:cl0] + ':' + str(lastEpoch) + \
+       f[cm0:cl1] + ':\"' + sys.argv[1] + '\"' + f[cm1:cl2] + ':\"' + sys.argv[1] + '\"' + \
+       f[cm2:cl3] + ':' + str(startEpoch) + \
+       f[cm3:cl4] + ":" + sys.argv[2] + f[cm4:]
 
 if (len(sys.argv) == 4) and (sys.argv[3] == '-v'):
   print("VESprepareSendEvent.py:: prepared event:") 
@@ -46,3 +66,4 @@ fil.write(evnt)
 fil.close
 
 print("VESprepareSendEvent.py:: VES send event prepared")
+
